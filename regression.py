978e8bbfd26f03d29c1386a5b01278c28b115027
Whitey <s1031422@users.noreply.github.com>
2017-08-24 16:55:14 +0800

    Basic Tensorbroad

--- regression.py
+++ regression.py
@@ -5,26 +5,44 @@ import matplotlib.pyplot as plt
 tf.set_random_seed(1)
 np.random.seed(1)
 
+#Generate Data
 x=np.linspace(-1,1,100)[:,np.newaxis]
 noisy=np.random.normal(0,0.1,x.shape)
 y=2*np.power(x,4)+2*np.power(x,3)-2*x+noisy
 
+#Preview Data
 plt.scatter(x,y)
 plt.show()
 
-tfX=tf.placeholder(tf.float32,x.shape)
+#Input Layer
+with tf.name_scope('Input'):
+    tfX=tf.placeholder(tf.float32,x.shape)
 tfY=tf.placeholder(tf.float32,y.shape)
-l1=tf.layers.dense(tfX,10,tf.nn.relu)
-output=tf.layers.dense(l1,1)
-loss=tf.losses.mean_squared_error(tfY,output)
-opt=tf.train.GradientDescentOptimizer(0.5)
-train=opt.minimize(loss)
+
+#Hidden Layer
+l1=tf.layers.dense(tfX,10,tf.nn.relu,name="Layer")
+
+#Output Layer
+output=tf.layers.dense(l1,1,name="Output")
+
+#Compute Loss
+with tf.name_scope('Loss'):
+    loss=tf.losses.mean_squared_error(tfY,output)
+
+#Training
+with tf.name_scope('Train'):
+    opt=tf.train.GradientDescentOptimizer(0.5)
+    train=opt.minimize(loss)
+#Init Variable
 init=tf.global_variables_initializer()
 
 feed={tfX:x,tfY:y}
 plt.ion()
+
+#Learning
 with tf.Session() as sess:
     sess.run(init)
+    train_writer=tf.summary.FileWriter('log/regression',sess.graph)
     for i in range(1000):
         _,l,pred=sess.run([train,loss,output],feed_dict=feed)
         if i%50==0:
